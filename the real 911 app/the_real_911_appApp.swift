65f17ffbb6f2598b651ffec7136aadb5912a0006
karon bell <karonbell5218@gmail.com>
2022-05-14 00:49:08 -0400

    Initial Commit

---
+++ the real 911 app/the_real_911_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_real_911_appApp.swift
+//  the real 911 app
+//
+//  Created by Karon Bell on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct the_real_911_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
